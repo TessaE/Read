0efa22cdb47550ec758fa0487ddec1df369bf4d5
Tessa <t.elfrink@ccv.eu>
2025-01-22 17:29:36 +0100

    Make some initial changes to the layout and content

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Welcome, reader!";
+export const SITE_DESCRIPTION = "This is where I list my books";
